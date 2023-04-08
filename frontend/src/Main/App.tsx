511e698b8a78a9f2e521e8d4b5429d791b71e7c4
McKenna Staley <mnnorton@byu.edu>
2023-04-07 20:12:26 -0600

    fixed folders

---
+++ frontend/src/Main/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TopBanner from '../Blah';
+import MovieList from '../movies';
+
+function App() {
+  return (
+    <div className="App">
+      <TopBanner saying="Joel's Movies- Home" />
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
